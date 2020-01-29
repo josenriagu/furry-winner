6d9d74651dc8a06f09f4eb2f01e1b6f3b2ed671b
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-01-29 21:43:13 +0100

    fix(frontend): update .travis.yml - add mongodb services - add fallback uri for test database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,3 @@
-const connectionUri = process.env.NODE_ENV === 'staging' ? process.env.DATABASE_URI || "mongodb://localhost:27017/softcom" : process.env.DATABASE_TEST_URI;
+const connectionUri = process.env.NODE_ENV === 'staging' ? process.env.DATABASE_URI || "mongodb://localhost:27017/softcom" : process.env.DATABASE_TEST_URI || "mongodb://localhost:27017/softcomtest";
 
 module.exports = connectionUri;
