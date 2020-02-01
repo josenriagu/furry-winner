32fd245b75dbceb8cf2bc65b94903461c0bcf172
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-02-01 20:54:11 +0100

    feat(backend): create & implement answer endpoint - adjust question model - update controllers - update helper modules  - refactor mapper  - refactor authenticators  - add new validators  - update strings - remove jest timeout flag. now testing locally - update tests

--- resources/helpers/authenticators.js
+++ resources/helpers/authenticators.js
@@ -4,6 +4,19 @@ const secret = require("../../config/secrets");
 const genError = require("./error");
 const m = require("./messageStrings");
 
+// This helper function will serve both cases in restrict middleware
+function verify(req, res, next) {
+  const token = req.headers.authorization;
+  jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
+    if (err) {
+      next(genError(401, m.tokenInvalid));
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
+}
+
 module.exports = {
   generateToken(user) {
     const payload = {
@@ -17,15 +30,18 @@ module.exports = {
   },
   restrict(req, res, next) {
     const token = req.headers.authorization;
-    if (token) {
-      jwt.verify(token, secret.jwtSecret, (err, decodedToken) => {
-        if (err) {
-          next(genError(401, m.tokenInvalid));
-        } else {
-          req.decodedToken = decodedToken;
-          next();
-        }
-      });
+    const { method, originalUrl } = req;
+    const { id } = req.params;
+
+    // Let's permit specific GET methods for questions route, even if no token (pseudo-private);
+    if (method === "GET" && (originalUrl === `${m.qBase}` || `${m.qBase}/${id}`)) {
+      if (token) {
+        verify(req, res, next);
+      } else {
+        next();
+      }
+    } else if (token) {
+      verify(req, res, next);
     } else {
       next(genError(400, m.supplyToken));
     }
--- resources/routes/question.js
+++ resources/routes/question.js
@@ -3,7 +3,7 @@ const express = require("express");
 const controller = require("../controllers/question");
 
 const { restrict } = require("../helpers/authenticators");
-const { validateBody, validateQuestion, validateQuestionId } = require("../helpers/validators");
+const v = require("../helpers/validators");
 const m = require("../helpers/messageStrings");
 const genError = require("../helpers/error");
 const updater = require("../helpers/updater");
@@ -11,13 +11,25 @@ const updater = require("../helpers/updater");
 const router = express.Router();
 
 /* Get all questions */
+// Update: I made this endpoint pseudo-private by modifying restrict middleware ...
 router.get("/", restrict, async (req, res, next) => {
   try {
-    /* Token will let us strip author-specific field(s) like
-     * 'notification' from the question for a non-author
-     * Recall that at this point, we have access to the decodedToken in the req object
+    /*
+      Token will let us strip author-specific field(s) like
+      'notification' from the question for a non-author
+      Recall that at this point (if authenticated);
+      we have access to the decodedToken in the req object
      */
     const questions = await controller.findAll(req.decodedToken);
+    if (questions.length === 0) {
+      /*
+        At first, the collection may be empty, let's show a nice response instead
+        Got me thinking, what was the first question ever asked on Stack Overflow? haha!
+      */
+      return res.status(200).json({
+        message: m.nqRetrieved,
+      });
+    }
     return res.status(200).json({
       message: m.qsRetrieved,
       questions,
@@ -28,14 +40,17 @@ router.get("/", restrict, async (req, res, next) => {
 });
 
 /* Get question by id */
-router.get("/:id", restrict, validateQuestionId, async (req, res, next) => {
+// Update: I made this endpoint pseudo-private by modifying restrict middleware ...
+router.get("/:id", restrict, v.validateQuestionId, async (req, res, next) => {
   try {
-    /* Token will let us strip author-specific field(s) like
-     * 'notification' from the question for a non-author
-     * Recall that at this point, we have access to the decodedToken in the   req object
-     * Recall also that we have access to the question bearing the id;
+    /*
+      Token will let us strip author-specific field(s) like
+      'notification' from the question for a non-author
+      Recall that at this point (if authenticated);
+      we have access to the decodedToken in the req object
+      Recall also that we have access to the question bearing the id;
      */
-    const question = await controller.find(req.question, req.decodedToken);
+    const question = await controller.refine(req.question, req.decodedToken);
     return res.status(200).json({
       message: m.qRetrieved,
       question,
@@ -46,13 +61,15 @@ router.get("/:id", restrict, validateQuestionId, async (req, res, next) => {
 });
 
 /* Post new question */
-router.post("/", restrict, validateBody, validateQuestion, async (req, res, next) => {
+router.post("/", restrict, v.validateBody, v.validateQuestion, async (req, res, next) => {
   try {
-    const question = await controller.save({
+    const savedQuestion = await controller.save({
       question: req.body.question,
       // At this point, we have access to the user's id from the decoded token
       userId: req.decodedToken.sub,
     });
+    // Let's add more context to the question
+    const question = await controller.refine(savedQuestion, req.decodedToken);
     return res.status(201).json({
       message: m.qCreated,
       question,
@@ -63,19 +80,41 @@ router.post("/", restrict, validateBody, validateQuestion, async (req, res, next
 });
 
 /* Upvote or Downvote a question */
-router.post("/:id/vote", restrict, validateBody, validateQuestionId, async (req, res, next) => {
+router.post("/:id/vote", restrict, v.validateBody, v.validateQuestionId, v.validateVote, async (req, res, next) => {
   try {
-    const { type } = req.body;
-    if (type && (type === "up" || type === "down")) {
-      /* Recall that at this point, we have access to the question bearing the id;
-       */
-      await updater.vote(req.question, "question", req.body.type);
-      return res.status(200).json({ message: m.voteUpdated });
-    }
-    if (type && type !== "up" && type !== "down") {
-      return next(genError(400, m.invalidType));
-    }
-    return next(genError(400, m.missingFields));
+    /*
+      Recall that at this point, we have access to the question bearing the id;
+    */
+    await updater.vote(req.question, "question", req.body.type);
+    return res.status(200).json({ message: m.voteUpdated });
+  } catch (error) {
+    return next(genError(500, error.message));
+  }
+});
+
+/* Answer a question */
+router.post("/:id/answer", restrict, v.validateBody, v.validateQuestionId, v.validateAnswer, async (req, res, next) => {
+  try {
+    /*
+      At this point, having passed all middleware checks,
+      we have access to;
+       - the answer in the request body;
+       - the userId from the decodedToken, now in the request object
+       - the question to be answered, now in the request object
+    */
+    const { answer } = req.body;
+    // Returned question is an instance of Mongoose Model
+    const savedQuestionModel = await controller.saveAnswer(
+      answer,
+      req.decodedToken.sub,
+      req.question,
+    );
+    // Let's add more context to the returned question object
+    const question = await controller.refine(savedQuestionModel.toObject(), req.decodedToken);
+    return res.status(201).json({
+      message: m.aCreated,
+      question,
+    });
   } catch (error) {
     return next(genError(500, error.message));
   }
